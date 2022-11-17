f0e7572e19e7fff751b03cff7781d9d949bae283
KingNelx <tapiajonel606@gmail.com>
2022-11-17 08:42:50 +0800

    added bootstrap 5

--- src/Components/HomeLogin.jsx
+++ src/Components/HomeLogin.jsx
@@ -3,6 +3,13 @@ import { Link } from "react-router-dom"
 const HomeLogin = () => {
     return (
         <div>
+            <nav class="navbar bg-light">
+                <div class="container">
+                    <a class="navbar-brand" href="#">
+                        <img src="/docs/5.2/assets/brand/bootstrap-logo.svg" alt="Bootstrap" width="30" height="24" />
+                    </a>
+                </div>
+            </nav>
             <div class="container text-center mt-5 py-5 d-flex justify-content-center">
                 <div class="row">
                     <div class="col">

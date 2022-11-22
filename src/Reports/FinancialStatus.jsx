f4e031258090bd2453ce37c9a6b294172bcd51cd
KingNelx <tapiajonel606@gmail.com>
2022-11-22 11:06:37 +0800

    added cards in financial reports

--- src/Reports/FinancialStatus.jsx
+++ src/Reports/FinancialStatus.jsx
@@ -21,26 +21,26 @@ const FinancialStatus = () => {
     <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
 </div> */}
-            <div class="container text-center mt-5">
+            <div class="container text-center mt-5 px-5 py-5">
                 <div class="row">
                     <div class="col">
                         <div class="card" >
-                            <img src="..." class="card-img-top" alt="..." />
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            <i class="fa-sharp fa-solid fa-chalkboard-user fa-2xl"></i>
+                            <div class="card-body shadow-lg">
+                                <h5 class="card-title">First Year</h5>
+                             
+                                <a href="#" class="btn btn-outline-primary">View</a>
                             </div>
                         </div>
                     </div>
                     <div class="col">
                         <div class="col">
                             <div class="card">
-                                <img src="..." class="card-img-top" alt="..." />
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                                <i class="fa-sharp fa-solid fa-chalkboard-user fa-2xl"></i>
+                                <div class="card-body shadow-lg">
+                                    <h5 class="card-title">Second Year</h5>
+                                 
+                                    <a href="#" class="btn btn-outline-primary">View</a>
                                 </div>
                             </div>
                         </div>
@@ -48,11 +48,11 @@ const FinancialStatus = () => {
                     <div class="col">
                         <div class="col">
                             <div class="card">
-                                <img src="..." class="card-img-top" alt="..." />
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                                <i class="fa-sharp fa-solid fa-chalkboard-user fa-2xl"></i>
+                                <div class="card-body shadow-lg">
+                                    <h5 class="card-title">Third Year</h5>
+                                  
+                                    <a href="#" class="btn btn-outline-primary">View</a>
                                 </div>
                             </div>
                         </div>
@@ -60,11 +60,11 @@ const FinancialStatus = () => {
                     <div class="col">
                         <div class="col">
                             <div class="card">
-                                <img src="..." class="card-img-top" alt="..." />
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                                <i class="fa-sharp fa-solid fa-chalkboard-user fa-2xl"></i>
+                                <div class="card-body shadow-lg">
+                                    <h5 class="card-title">Fourth Year</h5>
+                                  
+                                    <a href="#" class="btn btn-outline-primary">View</a>
                                 </div>
                             </div>
                         </div>
--- src/Reports/Scholarship.jsx
+++ src/Reports/Scholarship.jsx
@@ -1,17 +1,19 @@
+import { Link } from "react-router-dom"
+
 const Scholarship = () => {
-    return ( 
+    return (
         <div>
-             <div>
-            <nav class="navbar bg-light shadow-lg">
-                <div class="container-fluid">
-                    <a class="navbar-brand">Students Financial Status</a>
-                    <form class="d-flex" role="search">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                        <Link to = "/admintopNav" class="btn btn-outline-success" type="submit">Back</Link>
-                    </form>
-                </div>
-            </nav>
-            {/* 
+            <div>
+                <nav class="navbar bg-light shadow-lg">
+                    <div class="container-fluid">
+                        <a class="navbar-brand">Students Financial Status</a>
+                        <form class="d-flex" role="search">
+                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                            <Link to="/admintopNav" class="btn btn-outline-success" type="submit">Back</Link>
+                        </form>
+                    </div>
+                </nav>
+                {/* 
             <div class="card" style="width: 18rem;">
   <img src="..." class="card-img-top" alt="...">
   <div class="card-body">
@@ -20,21 +22,10 @@ const Scholarship = () => {
     <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
 </div> */}
-            <div class="container text-center mt-5">
-                <div class="row">
-                    <div class="col">
-                        <div class="card" >
-                            <img src="..." class="card-img-top" alt="..." />
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
+                <div class="container text-center mt-5">
+                    <div class="row">
                         <div class="col">
-                            <div class="card">
+                            <div class="card" >
                                 <img src="..." class="card-img-top" alt="..." />
                                 <div class="card-body">
                                     <h5 class="card-title">Card title</h5>
@@ -43,27 +34,39 @@ const Scholarship = () => {
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col">
                         <div class="col">
-                            <div class="card">
-                                <img src="..." class="card-img-top" alt="..." />
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                            <div class="col">
+                                <div class="card">
+                                    <img src="..." class="card-img-top" alt="..." />
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col">
                         <div class="col">
-                            <div class="card">
-                                <img src="..." class="card-img-top" alt="..." />
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                            <div class="col">
+                                <div class="card">
+                                    <img src="..." class="card-img-top" alt="..." />
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="col">
+                                <div class="card">
+                                    <img src="..." class="card-img-top" alt="..." />
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -71,8 +74,7 @@ const Scholarship = () => {
                 </div>
             </div>
         </div>
-        </div>
-     );
+    );
 }
- 
+
 export default Scholarship;
